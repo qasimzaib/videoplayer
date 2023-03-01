062838d7a789f0bba21218657093381ab955501f
qasimzaib <zaib_qasim@hotmail.com>
2023-03-01 11:45:08 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from 'react-native';
+import VideoPlayer from "./components/VideoPlayer";
+
+const App = () => {
+	return (
+		<View>
+			<VideoPlayer />
+		</View>
+	)
+}
+
+export default App;
---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+const VideoPlayer = () => {
+	return (
+		<View>
+			<Text>This is a video Player</Text>
+		</View>
+	)
+}
+
+export default VideoPlayer;
