c9c322aaa9d2864f0e4998025995c6c4d22a9fba
qasimzaib <zaib_qasim@hotmail.com>
2023-03-01 14:27:38 +0500

    Basic POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { View } from 'react-native';
-import VideoPlayer from "./components/VideoPlayer";
+import React, {useEffect} from 'react';
+import {View, PermissionsAndroid} from 'react-native';
+import VideoPlayer from './components/VideoPlayer';
+
+const requestStoragePermission = async () => {
+	try {
+		const granted = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+			{
+				title: 'Access to storage required',
+				message: 'Would you like to allow this access?',
+				buttonNeutral: 'Ask Me Later',
+				buttonNegative: 'Cancel',
+				buttonPositive: 'OK',
+			},
+		);
+		if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+			console.log('You can use the app');
+		} else {
+			console.log('Storage permission denied');
+		}
+	} catch (err) {
+		console.warn(err);
+	}
+};
 
 const App = () => {
+	useEffect(() => {
+		requestStoragePermission();
+	}, []);
 	return (
 		<View>
 			<VideoPlayer />
 		</View>
-	)
-}
+	);
+};
 
 export default App;
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,12 +1,48 @@
-import React from "react";
-import { Text, View } from 'react-native';
+import {useEffect, useState} from 'react';
+import {Text, SafeAreaView} from 'react-native';
+import RNFS from 'react-native-fs';
+import Video from 'react-native-video';
+
+const folderPath = RNFS.DownloadDirectoryPath + '/salaar';
+
+const fp = folderPath;
 
 const VideoPlayer = () => {
+	const [files, setFiles] = useState([]);
+
+	const makeDirectory = async folderPath => {
+		await RNFS.mkdir(folderPath); //create a new folder on folderPath
+	};
+
+	const getFileList = async (path: any) => {
+		const reader = await RNFS.readdir(path);
+		const files = [];
+		reader.map(item => {
+			files.push(fp + '/' + item);
+		});
+		setFiles(files);
+	};
+
+	useEffect(() => {
+		makeDirectory(folderPath);
+		getFileList(folderPath);
+	}, []);
+
 	return (
-		<View>
-			<Text>This is a video Player</Text>
-		</View>
-	)
-}
+		<SafeAreaView>
+			{files.length > 0 && (
+				<Video
+					source={{
+						uri: files[2],
+					}}
+					controls={true}
+					repeat={true}
+					paused={false}
+					style={{width: 300, height: 300}}
+				/>
+			)}
+		</SafeAreaView>
+	);
+};
 
 export default VideoPlayer;
